aa17daf1b85f5248be2ef45afd705fbc33202732
Shaik Nazeemuddin <nazeemoddeen@gmail.com>
2026-07-31 15:42:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import bcrypt from 'bcryptjs'
 //const bcrypt = require("bcryptjs");
 import jwt from 'jsonwebtoken'
 //const jwt = require("jsonwebtoken");
-import cors from "cors";
+
 
 const app = express()
 
@@ -22,11 +22,14 @@ import { fetchUsers, saveUser, usersDetails as users } from './railwaypostgress.
 
 app.use(express.json())
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// Enable CORS if your React app is on a different origin
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins for simplicity in development
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  // ✅ FIX: Add Authorization here
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello SK Nazeemuddin')
